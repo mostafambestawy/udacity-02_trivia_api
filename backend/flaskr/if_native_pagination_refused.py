7fcbc3e44fb59b12a767524efd999f3b7e7ab358
shimaa <engshimaaabdelwahab@gmail.com>
2020-11-30 18:25:31 +0200

    final

---
+++ backend/flaskr/if_native_pagination_refused.py
@@ -0,0 +1,8 @@
+QUESTIONS_PER_PAGE = 10
+
+
+def paginate(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page-1)*QUESTIONS_PER_PAGE
+    end = start+QUESTIONS_PER_PAGE
+    return [q.format() for q in selection][start:end]
